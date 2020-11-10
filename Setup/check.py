e6b40e661babaef76dfa7015531a631721ed4745
programmingAthlete <luca.bonamino@hotmail.com>
2020-11-10 17:25:06 +0100

    correction setup

--- Setup/check.py
+++ Setup/check.py
@@ -1,9 +1,17 @@
 import Setup.setup as setup
-import Includes.includes as includes
+#import Includes.includes as includes
 from importlib import import_module
 import os
-import sys 
+import subprocess
+import sys
 import time
+import io
+
+
+def command(commands,cmd):
+    '''Generalise the Unix <-> Windows commans'''
+    return commands[cmd][os.name]
+
 
 def pip_installation():
     '''
@@ -94,40 +102,51 @@ def check(args):
 
     versionList = []
     version = sys.version.split(" ")[0][:3]
+    if os.name == 'nt':
+        os.system('python --version > tempFile')
+        pythonVersion = open('tempFile', 'r').readline().split("Python ")[1].strip('\n')
+        os.system('clr')
+        os.system('del tempFile')
+    else:
+        #stream = io.StringIO()
+        #sys.stdout = stream
+        #os.system('python --version')
+        os.system('python --version "$1" > tempFile 2>&1')
+        pythonVersion = open('tempFile', 'r').readline().split("Python ")[1].strip('\n')
+        os.system('clear')
+        os.system('rm tempFile')
+
+    if pythonVersion == version:
+        pipVersion = ''
+    else:
+        if version[0] == '3':
+            pipVersion = '3'
+        else:
+            pipVersion = '2'
     if len(exceptions) > 0:
         print("[-] The following libraries are missing")
         time.sleep(1)
         for item in exceptions:
             print('\t%s' % item)
         print("\n")
-        if os.name == 'nt':
-            os.system('pip --version > tempFile')
-        else:
-            os.system('echo $(pip --version) >> tempFile')
-        pipVersion = args[0][-3:]
-        if version == pipVersion:
-            print("[+] Your pip default version corresponds with the python default version")
-            version = ""
-        else:
-            print("[-] Your pip default version does not correspond with your python default version")
-            print('''To install the packages needed you need to specify the python version you are using:
-                    your pip default version:           %s
+        print('''To install the packages needed you need to specify the python version you are using:
+                    your pip command version:       %s
                     python version you are using:   %s
 you have to specify the phyton version on the pip command to install the packages
             Example:
-                        pip%s install 'package name' \n''' % (str(pipVersion), str(version), str(version)))
+                        pip%s install 'package name' \n''' % (str(pipVersion), str(version), str(pipVersion)))
         show = '''To install the packages manually:\n
                         \t pip%s install 'package name'\n
     If you want the program to install the packages automatically, the following commands will be performed''' % ( str(version.split(".")[0]))
         print(show)
         for item in exceptions:
-            print('\tpip%s install %s' % (str(version), item))
+            print('\tpip%s install %s' % (str(pipVersion), item))
 
         a = str(input('Do you want to install the packages atomatically???? y/n '))
         if str(a) == 'y':
             for item in exceptions:
                 try:
-                    cmd = 'pip%s install %s' % (str(version), item)
+                    cmd = 'pip%s install %s' % (str(pipVersion), item)
                     os.system(cmd)
 
                 except:
